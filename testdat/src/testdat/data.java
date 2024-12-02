df7f9b41bc6ac42441d70e622c230fb379910014
Shobhahs <shobha26.hs@gmail.com>
2024-12-02 13:15:45 +0530

    shwetha

--- testdat/src/testdat/data.java
+++ testdat/src/testdat/data.java
@@ -4,10 +4,10 @@ public class data {
 
 	public static void main(String[] args) {
 	
-		System.out.println("The system is calculated suuucessfully-1");
-		System.out.println("The system is calculated suuucessfully-2");
-		System.out.println("The system is calculated suuucessfully-1-2-3");
-		System.out.println("The system is calculated suuucessfully-3");
+		System.out.println("The system is calculated suuucessfully");
+		System.out.println("The system is calculated suuucessfully");
+		System.out.println("The system is calculated suuucessfully");
+		System.out.println("The system is calculated suuucessfully");
 		
 
 	}
