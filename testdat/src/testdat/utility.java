7197ce192de0949b463c9726b13dbec6875621b9
Shobhahs <shobhashivarudraiah@gmail.com>
2024-12-02 13:55:34 +0530

    adding the utility

---
+++ testdat/src/testdat/utility.java
@@ -0,0 +1,15 @@
+package testdat;
+
+public class utility {
+
+	public static void main(String[] args) {
+	
+		
+		// 1. select method 
+		// click method 
+		// save method 
+		// table redaing 
+
+	}
+
+}
