483940f85082c0374b72f9d7f0011c91466a5a0d
shobha-hs <84800171+shobha-hs@users.noreply.github.com>
2024-12-02 13:36:10 +0530

    pull request
    Signed-off-by: shobha-hs <84800171+shobha-hs@users.noreply.github.com>

--- testdat/src/testdat/data.java
+++ testdat/src/testdat/data.java
@@ -4,10 +4,10 @@ public class data {
 
 	public static void main(String[] args) {
 	
-		System.out.println("The system is calculated suuucessfully");
-		System.out.println("The system is calculated suuucessfully");
-		System.out.println("The system is calculated suuucessfully-1-2");
-		System.out.println("The system is calculated suuucessfully");
+		System.out.println("The system is calculated suuucessfully-pull");
+		System.out.println("The system is calculated suuucessfully-pull2");
+		System.out.println("The system is calculated suuucessfully-1-2-pull3456789");
+		System.out.println("The system is calculated suuucessfully-iotyuiop");
 
 	}
 
